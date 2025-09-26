5bb45756127a3c6892322ec79c21c43bc1fd28f4
Rohan-Codes702 <rohanmane702@gmail.com>
2025-09-26 18:42:23 +0530

    commit

---
+++ Module/App.js
@@ -0,0 +1,2 @@
+const add=require('./Math');
+console.log(add(5,1));
---
+++ Module/Math.js
@@ -0,0 +1,11 @@
+function addition(a,b){
+  return a + b;
+    
+}
+
+const substraction=(a,b)=>{
+    return a - b;
+}
+
+
+module.exports=addition;
