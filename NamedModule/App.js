9d2251dd3d8f640913d5090a14793e3d044aa99f
Rohan-Codes702 <rohanmane702@gmail.com>
2025-09-26 20:25:38 +0530

    commit

---
+++ NamedModule/App.js
@@ -0,0 +1,8 @@
+const {add,sub,multi,div}=require('./Math');
+
+console.log("Addition of number",add(1,2));
+console.log("Substraction of number",sub(1,2));
+console.log("Multiplication of number",multi(1,2));
+console.log("Division of number",div(1,2));
+
+
