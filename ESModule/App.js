525cbe2fb26a4e64b1db44a7115a76e7c4092351
Rohan-Codes702 <rohanmane702@gmail.com>
2025-10-03 20:38:49 +0530

    commit

---
+++ ESModule/App.js
@@ -0,0 +1,11 @@
+//const {multi,add,sub,div}=require('./Math.js');
+
+import {multi}from "./Math.js";
+import {add}from "./Math.js";
+import {sub}from "./Math.js";
+import {div}from "./Math.js";
+
+console.log("Additon of two number",add(5,5));
+console.log("Substraction of two number",sub(5,5));
+console.log("MultiPliaction of two number",multi(5,5));
+console.log("Division of two number",div(5,5));
---
+++ ESModule/Math.js
@@ -0,0 +1,18 @@
+export const add =(a,b)=>{
+return a+b;
+}
+
+export const sub=(a,b)=>{
+    return a-b;
+}
+
+export const multi=(a,b)=>{
+    return a*b;
+}
+
+export const div=(a,b)=>{
+    return a/b;
+}
+
+//module.exports= {multi,add,sub,div};
+// export default multi;
